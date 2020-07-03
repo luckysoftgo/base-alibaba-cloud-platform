6f0c99bf532ae6bba62dda9880955d484e8d1f12
luckysoftgo <supingemail@126.com>
2020-07-03 14:56:43 +0800

    提交修改...

--- app-cloud-api-demo/bin/start-prod.sh
+++ app-cloud-api-demo/bin/start-prod.sh
@@ -4,4 +4,8 @@ cd $CURR_DIR
 if [ ! -d "../logs" ]; then
   mkdir ../logs
 fi
+rm -f tpid
 nohup java -jar ../app-cloud-api-demo-1.0.0-RELEASE.jar --server.port=56789 --spring.profiles.active=prod 1>/dev/null 2>> ../logs/nohup-err.out &
+echo $! > tpid
+echo Project Start Success!
+
