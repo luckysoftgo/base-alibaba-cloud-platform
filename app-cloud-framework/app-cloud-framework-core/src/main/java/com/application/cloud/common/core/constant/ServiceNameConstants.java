18929e70f80dd4353c0d6f8bdd39cc393d6f4f89
luckysoftgo <supingemail@126.com>
2020-07-15 10:49:02 +0800

    提交修改....

--- app-cloud-framework/app-cloud-framework-core/src/main/java/com/application/cloud/common/core/constant/ServiceNameConstants.java
+++ app-cloud-framework/app-cloud-framework-core/src/main/java/com/application/cloud/common/core/constant/ServiceNameConstants.java
@@ -10,10 +10,10 @@ public class ServiceNameConstants
     /**
      * 认证服务的serviceid
      */
-    public static final String AUTH_SERVICE = "cloud-auth";
+    public static final String AUTH_SERVICE = "app-cloud-auth";
 
     /**
      * 系统模块的serviceid
      */
-    public static final String SYSTEM_SERVICE = "cloud-system";
+    public static final String SYSTEM_SERVICE = "app-cloud-system";
 }
