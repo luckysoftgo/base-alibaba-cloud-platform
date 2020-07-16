c09f72034934b06a5db9501871c90183cac2c196
luckysoftgo <supingemail@126.com>
2020-07-16 16:56:29 +0800

    提交修改....

--- app-cloud-api-demo/src/main/java/com/application/cloud/demo/DemoApplication.java
+++ app-cloud-api-demo/src/main/java/com/application/cloud/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.application.cloud.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @NAME: DemoApplication
  * @DESC: DemoApplication类设计
  **/
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 	
 	public static void main(String[] args) {
+		
+		log.info("************************** 测试服务开始启动 ************************** ");
 		SpringApplication.run(DemoApplication.class, args);
+		log.info("************************** 测试服务启动完成 ************************** ");
 	}
 }
