26bd1ae2b793525acc627a881487fc3d3de99fa2
luckysoftgo <supingemail@126.com>
2020-07-17 17:18:17 +0800

    提交修改....

---
+++ app-cloud-api-demo/src/main/resources/sql/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (1, '张无忌', 88, 88, 88, '1', '2020-7-2 18:36:04', '1', '2020-7-2 18:36:04');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (2, '张无天', 99, 99, 99, '1', '2020-7-2 18:36:08', '1', '2020-7-2 18:36:26');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (3, '张无法', 89, 89, 89, '1', '2020-7-2 18:36:29', '1', '2020-7-2 18:36:52');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (4, '张无边', 98, 99, 98, '1', '2020-7-2 18:36:56', '1', '2020-7-2 18:37:16');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (5, '李无忌', 88, 88, 88, '1', '2020-7-2 18:36:04', '1', '2020-7-2 18:36:04');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (6, '李无天', 99, 99, 99, '1', '2020-7-2 18:36:08', '1', '2020-7-2 18:36:26');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (7, '李无法', 89, 89, 89, '1', '2020-7-2 18:36:29', '1', '2020-7-2 18:36:52');
+INSERT INTO `cloud_test_score` (`user_id`, `user_name`, `chinese_socre`, `math_socre`, `english_socre`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (8, '李无边', 98, 99, 98, '1', '2020-7-2 18:36:56', '1', '2020-7-2 18:37:16');
+
