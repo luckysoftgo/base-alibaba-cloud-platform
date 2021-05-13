ea5ce3299077c4b42f7861b53514b4016ed91762
luckysoftgo <supingemail@126.com>
2021-05-13 11:33:47 +0800

    业务中远程调用的参数校验 ...

---
+++ app-cloud-framework/app-cloud-framework-basic/src/main/java/com/application/cloud/basic/ValidationBeanHandler.java
@@ -0,0 +1,57 @@
+package com.application.cloud.basic;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author ：admin
+ * @date ：2021-5-13
+ * @description: bean验证处理类
+ * @modified By：
+ * @version: 1.0.0
+ */
+public final class ValidationBeanHandler {
+	
+	/**
+	 * 实例验证工厂
+	 */
+	private static ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	
+	/**
+	 * 验证方法
+	 *
+	 * @param t
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> List<String> validate(T t) {
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(t);
+		List<String> messageList = new ArrayList<>();
+		for (ConstraintViolation<T> constraintViolation : constraintViolations) {
+			messageList.add(constraintViolation.getMessage());
+		}
+		return messageList;
+	}
+	
+	/**
+	 * 校验是否填了必填参数.
+	 *
+	 * @param t
+	 * @return
+	 * @throws RuntimeException
+	 */
+	public static <T> boolean validated(T t) throws RuntimeException {
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(t);
+		if (constraintViolations != null && constraintViolations.size() > 0) {
+			throw new RuntimeException(constraintViolations.iterator().next().getMessage());
+		}
+		return true;
+	}
+}
